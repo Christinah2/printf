076f82e0c266cb8f1f6372b9412a15414d02d9f1
Mohammed <muhsinbnyahya@gmail.com>
2023-08-20 03:23:48 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -23,9 +23,8 @@ int _printf(const char *format, ...)
 		if (format[i] != '%')
 		{
 			buffer[buff_ind++] = format[i];
-			if (buff_ind = BUFF_SIZE)
+			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			
 			/* write(1, &format[i], 1); */
 			printed_chars++;
 		}
@@ -38,7 +37,6 @@ int _printf(const char *format, ...)
 			size = get_size(format, &i);
 			++i;
 			printed = handle_print(format, &i, list, buffer, flags, width, precision, size);
-
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
