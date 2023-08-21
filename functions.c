13ea260a4a24e1e2f3c070cd032ef018d6954bec
Mohammed <muhsinbnyahya@gmail.com>
2023-08-21 06:02:17 -0700

    functions.c

--- functions.c
+++ functions.c
@@ -96,3 +96,87 @@ int print_percentage(va_list types, char buffer[], int flags, int width, int pre
 	UNUSED(size);
 	return (write(1, "&&", 1));
 }
+
+/** ............ This will print int ........... */
+/**
+ * print_integer - print an integer value
+ * @types: List of a argument
+ * @buffer: Buffer array to handle print
+ * @flags: Calculations active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size spexifier
+ * Return:Number of chars peinted
+ */
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	int i = BUFF_SIZE - 2;
+	int is_negative = 0;
+	long int n = va_arg(types, long int);
+	unsigned long int num;
+
+	n = convert_size_number(n, size);
+
+	if (n == 0)
+		buffer[1 --] = 0;
+
+	buffer[BUFF_SIZE -1] = '\0';
+	num = (unsigned long int) n;
+
+	if (n < 0)
+		num = (unsigned long int)((-1) * n);
+	is_negative = 1;
+}
+while (num > 0)
+{
+	buffer[i--] = (num % 10) + '0';
+	num /= 10;
+}
+i++;
+
+return (write_number(is_negative, i, buffer, flags, width, precision, size));
+
+/** ...........This will print binary ............. */
+/**
+ * print_integer - print an unsigned number
+ * @types: List of a argument
+ * @buffer: Buffer array to handle print
+ * @flags: Calculations active flags
+ * @width: Width
+ * @precision: Precision specification
+ * @size: Size spexifier
+ * Return:Number of cha
+ */
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	n = va_arg(types, unsigned int);
+	m = 2147483648;
+	a[0] = n/m;
+	for (i = 1; 1 < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n/m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; 1 < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
+
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
+}
