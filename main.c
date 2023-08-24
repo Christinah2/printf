55c3dee1d2d2fd8bae27df4b4c4939a1da12a593
Mohammed <muhsinbnyahya@gmail.com>
2023-08-24 04:14:40 -0700

    main.c

--- main.c
+++ main.c
@@ -22,7 +22,7 @@ _printf("Length:[%d, %i]\n", len, len);
 printf("Length:[%d, %i]\n", len2, len2);
 _printf("Negative:[%d]\n", -762534);
 printf("Negative:[%d]\n", -762534);
-_printf("Unsigned:[%u]\n", ui); 
+_printf("Unsigned:[%u]\n", ui);
 printf("Unsigned:[%u]\n", ui);
 _printf("Unsigned octal:[%o]\n", ui);
 printf("Unsigned octal:[%o]\n", ui);
