614f2386801da0a46648cbcafd2757b0d71ee15c
Mohammed <muhsinbnyahya@gmail.com>
2023-08-24 16:03:44 -0700

    print_size.c

--- print_size.c
+++ print_size.c
@@ -12,20 +12,18 @@
 
 int get_size(const char *format, int *a)
 {
-int size = 0;
-int curr_a = *a + 1;
+	int curr_a = *a + 1;
+	int size = 0;
 
-if (format[curr_a] == 'l')
-size = S_LONG;
-<<<<<<< HEAD
-while (format[curr_a] == 'h')
-=======
-else if (format[curr_a] == 'h')
->>>>>>> 30f0358700032905e2db02d1ccfc9f50afcf0f69
-size = S_SHORT;
-if (size == 0)
-*a = curr_a - 1;
-else
-*a = curr_a;
-return (size);
+	if (format[curr_a] == 'l')
+		size = S_LONG;
+	else if (format[curr_a] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*a = curr_a - 1;
+	else
+		*a = curr_a;
+
+	return (size);
 }
