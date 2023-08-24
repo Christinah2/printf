89ae40edd2d7e0a042eba162ef8188b4e92ff8c3
Christinah2 <christijoj@gmail.com>
2023-08-24 09:03:25 -0700

    update

--- handle_print.c
+++ handle_print.c
@@ -11,7 +11,8 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[], int flags, int width, int precision, int size)
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
--- print_functions2.c
+++ print_functions2.c
@@ -11,7 +11,8 @@
  * @size: Size specifier
  * Return: Number of chars printed.
  */
-int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_pointer(va_list types, char buffer[], int flags, int width, int
+precision, int size)
 {
 	char extra_c = 0, padd = ' ';
 	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
@@ -47,7 +48,8 @@ int print_pointer(va_list types, char buffer[], int flags, int width, int precis
 	ind++;
 
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length, width, flags, padd, extra_c, padd_start));
+	return (write_pointer(buffer, ind, length, width, flags, padd, extra_c,
+	padd_start));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
@@ -61,7 +63,8 @@ int print_pointer(va_list types, char buffer[], int flags, int width, int precis
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_non_printable(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_non_printable(va_list types, char buffer[], int flags, int width, int
+precision, int size)
 {
 	int i = 0, offset = 0;
 	char *str = va_arg(types, char *);
@@ -102,7 +105,8 @@ int print_non_printable(va_list types, char buffer[], int flags, int width, int 
  * Return: Numbers of chars printed
  */
 
-int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_reverse(va_list types, char buffer[], int flags, int width, int
+precision, int size)
 {
 	char *str;
 	int i, count = 0;
@@ -143,7 +147,8 @@ int print_reverse(va_list types, char buffer[], int flags, int width, int precis
  * @size: Size specifier
  * Return: Numbers of chars printed
  */
-int print_rot13string(va_list types, char buffer[], int flags, int width, int precision, int size)
+int print_rot13string(va_list types, char buffer[], int flags, int width, int
+precision, int size)
 {
 	char y;
 	char *str;
